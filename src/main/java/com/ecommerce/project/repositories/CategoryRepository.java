6ced130760b676264419e48fe33dd72df74f48a4
Aman Kandari <kandariaman24@gmail.com>
2024-11-17 03:46:47 +0530

    Added custom API Exception for category with duplicate category name

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Category findByCategoryName(String categoryName);
 }
